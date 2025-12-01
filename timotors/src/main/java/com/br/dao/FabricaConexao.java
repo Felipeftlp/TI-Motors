f6caf511c75e03b05450fb50805b8058123dc292
Felipeftlp <felipefreitas13@live.com>
2025-11-30 23:25:08 -0300

    refactor: novo setup pra rodar o projeto

--- timotors/src/main/java/com/br/dao/FabricaConexao.java
+++ timotors/src/main/java/com/br/dao/FabricaConexao.java
@@ -17,8 +17,8 @@ import java.sql.Statement;
  */
 public class FabricaConexao {
 
-    private static final String USER = "root";
-    private static final String PASS = "admin123";
+    private static final String USER = "admin";
+    private static final String PASS = "1234";
     private static final String URL  = "jdbc:mysql://127.0.0.1:3306/tiMotors";
     
        public static Connection getConnection() {
